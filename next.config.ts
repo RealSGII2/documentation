fc8e611e8120eade716a83c77dffd42e1eb9e1be
William <william@wwise.dev>
2025-03-04 22:20:51 -0600

    Testing deployment config to keep doc files

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,19 @@
 import createMDX from '@next/mdx';
 import type { NextConfig } from 'next';
 
+const DOCS = [
+    './src/docs/**/*',
+    './docs/**/*'
+]
+
 const nextConfig: NextConfig = {
     sassOptions: {
         silenceDeprecations: ["legacy-js-api", "color-functions", "global-builtin"],
+    },
+    outputFileTracingIncludes: {
+        '*': DOCS,
+        '/*': DOCS,
+        '/\\[module\\]/\\[\\.\\.\\.path\\]': DOCS
     }
 };
 
